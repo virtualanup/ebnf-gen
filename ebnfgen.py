4a8f2d1a5c8c915eaa08c8e7544c0f83f6619334
Anup Pokhrel <virtualanup@gmail.com>
2016-10-27 23:48:42 -0400

    Derive follow set also

--- ebnfgen.py
+++ ebnfgen.py
@@ -10,24 +10,31 @@ class GrammarParser:
         self.nt = set()
         self.productions = defaultdict(list)
 
+        self.non_entry_nt = set()
+
         self.first = defaultdict(set)
         self.follow = defaultdict(set)
 
         self.symbols = set()
         self.eps = defaultdict(lambda: False)
+        self.nt_order = []
+
         grammar = grammar.replace("→", "->")
-        grammar = grammar.replace("ε", "epsilon")
+        grammar = grammar.replace("ε", self.epsilon)
 
         for production in filter(lambda x: "->" in x, grammar.split("\n")):
             nt, rhs = production.split("->")
             nt = nt.strip()
+            if nt not in self.nt_order:
+                self.nt_order.append(nt)
+
             self.nt.add(nt)
             self.symbols.add(nt)
             for s_prod in rhs.split("|"):
                 cur_prod = []
                 for symbol in s_prod.split():
                     symbol = symbol.strip()
-
+                    self.non_entry_nt.add(symbol)
                     cur_prod.append(symbol)
 
                     if symbol == self.epsilon:
@@ -38,10 +45,14 @@ class GrammarParser:
                 self.productions[nt].append(cur_prod)
 
         self.terminals = self.symbols - self.nt
+
+        self.start_symbols = self.nt - self.non_entry_nt
+
         for terminal in self.terminals:
             self.first[terminal].add(terminal)
-        changed = True
 
+        # Calculate the first set
+        changed = True
         while changed:
             changed = False
             for nt in self.nt:
@@ -49,22 +60,63 @@ class GrammarParser:
                     found = False
                     for symbol in prod:
                         if self.epsilon not in self.first[symbol]:
-                            if self.epsilon not in self.first[symbol]:
-                                if len(self.first[symbol] - self.first[nt]) > 0:
-                                    changed = True
+                            if len(self.first[symbol] - self.first[nt]) > 0:
+                                changed = True
                                 self.first[nt] |= self.first[symbol]
-                                break
+                            break
                     else:
                         if self.epsilon not in self.first[nt]:
-                            print(".")
                             self.first[nt].add(self.epsilon)
                             changed = True
 
-        # Now, get the first and follow set
+        # Calculate the follow set
+        # The start_symbols represent the non terminals that don't appear in rhs
+        if not self.start_symbols:
+            self.start_symbols.add(self.nt_order[0])
+
+        for nt in self.start_symbols:
+            # EOL can follow those symbols
+            self.follow[nt].add("$")
+
+        changed = True
+        while changed:
+            changed = False
+
+            for nt in self.nt:
+                for prod in self.productions[nt]:
+
+                    for symbol1, symbol2 in zip(prod, prod[1:]):
+                        # The first symbol must be non-terminal
+                        if symbol1 in self.nt:
+                            first_sym2 = self.first[symbol2] - set([self.epsilon])
+                            if len(first_sym2 - self.follow[symbol1]) > 0:
+                                changed = True
+                                self.follow[symbol1] |= first_sym2
+
+                    last_item = prod[-1]
+                    if last_item in self.nt and len(self.follow[nt] - self.follow[last_item]) > 0:
+                        changed = True
+                        self.follow[last_item] |= self.follow[nt]
+
+                    if len(prod) > 1:
+                        last = prod[-1]
+                        if self.epsilon in self.first[last]:
+                            second_last = prod[-2]
+                            if len(self.follow[nt] - self.follow[second_last]) > 0:
+                                changed = True
+                                self.follow[second_last] |= self.follow[nt]
+
+    def _print_set(self, pset):
+        # This is just some dirty hack to print the non terminals before terminals and in the same
+        # order that they appeared
+        for symbol, f_set in sorted(pset.items(), key=lambda x:self.nt_order.index(x[0]) if x[0] in self.nt_order else len(self.nt_order)+1):
+            print("{}\t:\t{}".format(symbol, ', '.join(sorted(f_set))))
 
     def print_first_set(self):
-        for symbol, f_set in self.first.items():
-            print("{}\t:\t{}".format(symbol, ', '.join(f_set)))
+        self._print_set(self.first)
+
+    def print_follow_set(self):
+        self._print_set(self.follow)
 
     def first_set(self):
         return self.first
@@ -81,3 +133,6 @@ if __name__ == "__main__":
         ebnf = GrammarParser(gf)
         print("\nFirst set\n")
         ebnf.print_first_set()
+
+        print("\n Follow set\n")
+        ebnf.print_follow_set()
